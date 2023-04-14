f05f7c5861394f84ae29ff4b8c1b88f1675578f0
Isabel Frade <isabelf@instemlss.co.uk>
2023-04-14 13:12:32 -0400

    outputs ATLAS, orgranism_part, description

--- atlasrequest.py
+++ atlasrequest.py
@@ -5,7 +5,6 @@ import ebisearch
 def expression_atlas_api(query_term):
     url = f"https://www.ebi.ac.uk/ebisearch/ws/rest/atlas-genes-differential?query={query_term}&size=10000&format=idlist"
     response = requests.get(url)
-
     # Check if the response was successful
     if response.status_code == 200:
         # Parse the response JSON
@@ -16,16 +15,27 @@ def expression_atlas_api(query_term):
         #print(idDict)
 
         # Save the response JSON to a file
-        with open(f"{query_term}.json", "wb") as file:
-            file.write(response.content)
-
-            with open(f"{query_term}_info.json", "w") as file:
-                for i in range(0,10):
-                    print(idDict[i])
-                    json.dump(ebisearch.get_entries( domain="atlas-genes-differential",
-                    entryids= idDict[i], fields="ATLAS,description,domain_source,name,organism_part"), file)
-            
+        idfromList = " "
+        with open(f"{query_term}.json", "w") as file:
+            file.write(ids)
         
+        with open(f"{query_term}_info.json", "w") as file:
+                #for i in range(0,100):
+                    i = 0
+                    x= idDict[i]
+                    retrieveurl= f"https://www.ebi.ac.uk/ebisearch/ws/rest/atlas-genes-differential?query={idDict[i]}&fields=organism_part,ATLAS,EMBL,ENTREZGENE,GO,INTERPRO,REFSEQ,TAXONOMY,id,description,name&format=json"
+                    #&filter=id:{x}
+                    ##EMBL,ENTREZGENE,GO,INTERPRO,REFSEQ,TAXONOMY
+                    #print(retrieveurl)
+                    response = requests.get(retrieveurl)
+                    retrieve = response.content.decode()
+                    #print(retrieve)
+                    json.dump(retrieve, file)
+                    """""
+                    json.dump(ebisearch.get_entries( domain="atlas-genes-differential",
+                    entryids= idDict[i], fields="organism_part,ATLAS,description,domain_source,name,"), file)
+                    """
+
         print(f"API request for query term '{query_term}' successful")
     else:
         print(f"API request for query term '{query_term}' failed with status code {response.status_code}")
@@ -33,3 +43,4 @@ def expression_atlas_api(query_term):
 #print(ebisearch.get_retrievable_fields(domain="atlas-genes-differential"))
   
 expression_atlas_api("brca1")
+#url = 'https://www.ebi.ac.uk/ebisearch/ws/rest/atlas-genes-differential?query={id}&size=1000&fields=organism_part,ATLAS&format=json'
