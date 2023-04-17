85344a87943e9e2c2b01f1906d31d5663f3b97f7
Isabel Frade <isabelf@instemlss.co.uk>
2023-04-17 11:12:32 -0400

    created atlas tsv parser

--- atlas.py
+++ atlas.py
@@ -6,30 +6,19 @@ from datetime import datetime
 import time
 import csv
 """
-This is a script that contains a function to create, 
-from these downloaded files, a single json file which contains
-a non-redundant set of pubmed records from all these files.
+This is a script to parse the .tsv file outputed by Expression ATLAS. 
+Input files can be gather here: https://www.ebi.ac.uk/gxa/home
 
+Things to address:
+output file name should probably be from user input. 
 """
-"""""
-Useful website: https://www.nlm.nih.gov/bsd/mms/medlineelements.html
-Stuff to fix:
-# What happens with revised versions of the same article?-revised versions have the same PMID! 
-# May need to use PHST- <date> [pubmed] to get a more recent year
-#adjust output name?
-# This probably can't handle subfolders at the moment
-# Right now it has only been tested with parser3.py and the folder in the same
-spot. Needs further testing there
-#add a part where it can get a pubMed element key as an argument, and then add it to subdata? 
-"""""
-
 def parserAtlas(file_name):
     t0= time.clock() #test code efficiency
     if len(file_name) < 2:
-        print("Error: parserPubmed(<file_name>,<hugo_ref>, <source>) is missing a value")
+        print("Error: parserPubmed(<file_name>) is empty")
         sys.exit()
     if not os.path.basename(file_name):
-        print("Error: The specified folder does not exist.")
+        print("Error: The specified file does not exist.")
         sys.exit()
     output_dir = os.path.dirname(file_name)
     outputFileName = file_name.split(".")[1]
@@ -61,14 +50,5 @@ def parserAtlas(file_name):
         sys.exit()
     t1 = time.clock()
     print("Time elapsed: ", t1 - t0)
-    # with open (pmidListdoc, "w", encoding ="utf8") as fp:
-    #     for item in pmidList:
-    #         fp.write("%s\n" % item)
-    #     print("done")
-    #     fp.close()
-    #     print(len(pmidList))
-    #print(len(data))
-#example run of parsePubmed. likely the folder path needs to be more specific
-#at this time, both brca1 & parser3 are in the same folder
+   
 parserAtlas('.\differentialResults.tsv')
-#parserPubmed("<folder_path>", "<hug_ref>", "<source>")
