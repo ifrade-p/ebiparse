d471764c0b00b069956b63ce14c9174511f3c82b
Isabel Frade <isabelf@instemlss.co.uk>
2023-04-21 18:07:28 -0400

    fixed ftp bugs

---
+++ currentfiles.py
@@ -0,0 +1,23 @@
+import json
+import ftplib
+from ftplib import FTP
+import os, sys, os.path
+import urllib.request
+from urllib.parse import urlparse
+from tqdm.auto import tqdm
+#This program goes to this directory: http://ftp.ebi.ac.uk/pub/databases/microarray/data/atlas/experiments/
+#and pulls files that end in .txt
+#RDF is the last folder checked. 
+"""
+Things to address:
+should the txt be what's pulled? 
+"""
+def get_atlas_files():
+    folder = os.listdir("atlas_files")
+    with open("atlas_experiments_current.txt", "w") as fp:
+        for x in tqdm(folder):
+            if x.startswith("E-"):
+                x= x.split(".")[0]
+                fp.write(x+"\n")
+    print("Done!")
+get_atlas_files()
--- ftp_atlas.py
+++ ftp_atlas.py
@@ -1,6 +1,6 @@
 import json
 import ftplib
-from ftplib import FTP
+from ftplib import FTP, error_perm
 import os, sys, os.path
 import urllib.request
 from urllib.parse import urlparse
@@ -27,29 +27,48 @@ def get_atlas_files(url):
     ftp.retrlines("LIST", atlasFolders.append)
     atlasFolders = [x.split()[-1] for x in atlasFolders if x.startswith("d")]
     atlasFolders = [f for f in atlasFolders if f.startswith("E-")]
+    currentfiles = []
+    newfiles = []
+    with open("atlas_experiments_current.txt", "r") as current:
+        currentfiles = [line.rstrip('\n') for line in current]
+
+    for i in range(len(atlasFolders)):
+        if atlasFolders[i] not in currentfiles:
+             newfiles.append(atlasFolders[i])
+    print(len(atlasFolders))
+    print(len(newfiles))
+    unaccessibleFiles = []
+    unaccessibleFilesCount=0
     #print(atlasFolders)
     #This for loop gets all the txt files in each folder.
     fp = open("atlas_experiments.txt", "a") #this file will contain list of experiments
-    for i in tqdm(range(len(atlasFolders))):
-        folder = atlasFolders[i]
+    for i in tqdm(range(len(newfiles)), "txt retrieval"):
+        folder = newfiles[i]
+        #print(folder)
+        try:
+            ftp.cwd(folder)
+            files = []
+            ftp.retrlines("NLST", files.append)
+            txt_files = [f for f in files if (f.endswith("idf.txt"))]
+            # download each txt file to the atlas_files folder
+            for txt_file in (txt_files):
+                if not os.path.exists(txt_file):
+                    with open(os.path.join("atlas_files", txt_file), "wb") as f:
+                        ftp.retrbinary("RETR " + txt_file, f.write)
+                        #print(txt_file)
+                        f.close()
+            ftp.cwd("..")
+        except error_perm as e:
+            unaccessibleFiles.append(folder)
+            unaccessibleFilesCount+=1
+            #print({e})
+            #print(folder)
+
         #print("\n",folder)
-        ftp.cwd(folder)        # get a list of all the txt files in the current folder
-        files = []
-        ftp.retrlines("NLST", files.append)
-        txt_files = [f for f in files if (f.endswith("idf.txt"))]
-
-        # download each txt file to the atlas_files folder
-        for txt_file in (txt_files):
-            if not os.path.exists("atlas_files\\"+txt_file):
-                print(txt_file)
-                with open(os.path.join("atlas_files", txt_file), "wb") as f:
-                    ftp.retrbinary("RETR " + txt_file, f.write)
-                    #print(txt_file)
-                    f.close()
-            fp.write("\n"+folder)
+        
         #print("\n"+folder)
-        ftp.cwd("..")
-
+        
+    print(unaccessibleFilesCount+ " inaccessible files")
     ftp.quit()
     fp.close()
     print("Done!")
